f128f10b3130247e5f43dec2fefa81dce004aa24
muxaffer786 <muxaffer786@gmail.com>
2026-01-07 22:18:25 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        /*
+
+
+         ...............................Java programming............................
+
+
+         */
+        System.out.println("Hello World!");
+        System.out.println("This is my first program!");
+        System.out.println("Let's Write Programs!");
+    }
+}
